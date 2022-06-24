0746afb6c0395479d0725ec8f3e41b5a61d5ab15
Adesaya Yemisi <yemisadesanya@gmail.com>
2022-06-24 14:59:48 +0100

    saving

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -22,20 +22,7 @@ const Dashboard = () => {
 					<div className="flex items-center justify-between">
 						<div className="flex items-center justify-start">
 							<button className="p-2 text-gray-600 rounded cursor-pointer lg:hidden ">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									className="w-6 h-6"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										stroke-width="2"
-										d="M4 6h16M4 12h16M4 18h16"
-									/>
-								</svg>
+								
 							</button>
 							<a href="#" className="flex items-center text-xl font-bold">
 								<span className="text-[#fff]">Logo</span>
