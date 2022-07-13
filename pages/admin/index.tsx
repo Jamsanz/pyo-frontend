ffe544e673e31311630273c57bfeab77f21eee66
Muhammad <jamiludeensani@gmail.com>
2022-07-13 14:31:16 +0100

    commiting to switch branch

---
+++ pages/admin/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AdminDashboardLayout from '../../components/AdminDashboardLayout';
+
+const Index = () => {
+  return (
+    <AdminDashboardLayout>
+
+    </AdminDashboardLayout>
+  )
+}
+
+export default Index;
