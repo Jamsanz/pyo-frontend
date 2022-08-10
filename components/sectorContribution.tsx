694ebf274719e5357da9818b05b5583899c3c6c5
Adesaya Yemisi <yemisadesanya@gmail.com>
2022-08-10 10:34:17 +0100

    added read more implementation to sector specific contribution section

--- components/sectorContribution.tsx
+++ components/sectorContribution.tsx
@@ -1,21 +1,12 @@
 import React, { useState } from "react";
 import { sectSpecific } from "../utils/constant";
 
-
-
 const SectorContribution = () => {
-
-  const [open, setOpen] = useState<boolean>(false);
   const [show, setShow] = useState<boolean>(false);
 
   const [moreText, setMoreText] = useState<any>();
 
   let readMoreText: any;
-
-  const handleShowModal = (e?: MouseEvent): void => {
-    e?.preventDefault();
-    setOpen(!open);
-  };
 
   const showMore = (id: number) => {
     setShow(true);
@@ -27,7 +18,6 @@ const SectorContribution = () => {
     return readMoreText;
   };
 
-
   return (
     <div className="lg:px-24 bg-[#fff] text-center mx-uato mt-8 md:mt-24">
       <h1 className=" px-5 md:px-20 my-4 pb-5 text-[30px] md:text-4xl font-bold text-center leading-tight text-[#000]">
@@ -35,61 +25,56 @@ const SectorContribution = () => {
       </h1>
 
       <div className="pb-8 container mx-auto flex flex-wrap flex-col md:flex-row text-black border-b-[1px] border-b-solid border-b-[#000] justify-center">
-        
-        {
-          sectSpecific.map((data:any) => {
-            return (
-              <div className="w-full md:w-3/6 xl:w-2/6 pt-4 pb-8 align-middle  px-2 md:px-8 text-[#000] text-left">
-                <div className="w-full text-center mx-auto">
-                  <img
-                    src={data?.img}
-                    className="rounded-lg mb-[20px] boxShadow2 w-[95%] mx-auto"
-                  />
-                </div>
-                <h3 className="monoFont text-[22px] font-bold text-center mx-auto md:pb-0 h-auto md:h-16">
-                  {data?.title}
-                </h3>
-                <span className="pt-2 text-[16px] text-[#000] font-[400] leading-[1.5rem] text-justify">
-                  {data?.text1.substring(0, 220)}
-                  
-                  {show && (
-                    <span>
-                      {moreText?.id === data?.id &&
-                        moreText?.text1?.substring(
-                          220,
-                          moreText?.text1.length - 1
-                        )}
-                    </span>
-                  )}
-                </span>
+        {sectSpecific.map((data: any) => {
+          return (
+            <div className="w-full md:w-3/6 xl:w-2/6 pt-4 pb-8 align-middle  px-2 md:px-8 text-[#000] text-left">
+              <div className="w-full text-center mx-auto">
+                <img
+                  src={data?.img}
+                  className="rounded-lg mb-[20px] boxShadow2 w-[95%] mx-auto"
+                />
+              </div>
+              <h3 className="monoFont text-[22px] font-bold text-center mx-auto md:pb-0 h-auto md:h-16">
+                {data?.title}
+              </h3>
+              <span className="pt-2 text-[16px] text-[#000] font-[400] leading-[1.5rem] text-justify">
+                {data?.text1.substring(0, 220)}
 
-                {moreText?.id === data?.id && show ? (
-                  <span
-                    className="cursor-pointer text-lg text-[#000] font-[400] leading-[1.5rem] text-center"
-                    onClick={() => setShow(!show)}
-                  >
-                    <p className="mt-2 text-[17px] font-bold text-black text-left transform transition hover:text-[#14084e] duration-300 ease-in-out">
-                      Show less
-                    </p>
-                  </span>
-                ) : (
-                  <span
-                    className="cursor-pointer text-lg text-[#14044E] font-[400] leading-[1.5rem] "
-                    onClick={() => showMore(data?.id!)}
-                  >
-                    <span
-                      className="text-[17px] font-bold text-black transform transition hover:text-[#14084e] duration-300 ease-in-out"
-                    >
-                    &nbsp; ...continue reading ...
-                    </span>
+                {show && (
+                  <span>
+                    {moreText?.id === data?.id &&
+                      moreText?.text1?.substring(
+                        220,
+                        moreText?.text1.length - 1
+                      )}
                   </span>
                 )}
+              </span>
 
-                <div className="text-center m-auto"></div>
-              </div>
-            );
-          })
-        }
+              {moreText?.id === data?.id && show ? (
+                <span
+                  className="cursor-pointer text-lg text-[#000] font-[400] leading-[1.5rem] text-center"
+                  onClick={() => setShow(!show)}
+                >
+                  <p className="mt-2 text-[17px] font-bold text-black text-left transform transition hover:text-[#14084e] duration-300 ease-in-out">
+                    Show less
+                  </p>
+                </span>
+              ) : (
+                <span
+                  className="cursor-pointer text-lg text-[#14044E] font-[400] leading-[1.5rem] "
+                  onClick={() => showMore(data?.id!)}
+                >
+                  <span className="text-[17px] font-bold text-black transform transition hover:text-[#14084e] duration-300 ease-in-out">
+                    &nbsp; ...continue reading ...
+                  </span>
+                </span>
+              )}
+
+              <div className="text-center m-auto"></div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
